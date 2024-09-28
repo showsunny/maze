2703173e802a2585ff50308b97eb64f9cc7aa195
showsunny <95205568+showsunny@users.noreply.github.com>
2024-09-28 19:13:28 +0800

    更改墙壁顺序

--- primsAlgorithm.py
+++ primsAlgorithm.py
@@ -55,18 +55,22 @@ class Cell:
         self.visited = visited
         self.highlited = highlited
     def draw(self):
-        #rect = pygame.Rect(100, 100, 200, 150)
+
         if self.visited:
-            pygame.draw.rect(SCREEN, DARK_GREEN, pygame.Rect(self.row*W, self.col*W, W, W))
+            pygame.draw.rect(SCREEN, DARK_GREEN, pygame.Rect(self.col*W, self.row*W, W, W))
         if self.highlited:
-            pygame.draw.rect(SCREEN, WHITE, pygame.Rect(self.row*W, self.col*W, W, W))
+            pygame.draw.rect(SCREEN, WHITE, pygame.Rect(self.col*W, self.row*W, W, W))
         if self.lines[0]:
+            #top
             pygame.draw.line(SCREEN, GREEN, (self.col*W, self.row*W), (self.col*W+W+T, self.row*W), outlineThickness)
         if self.lines[1]:
+            #right
             pygame.draw.line(SCREEN, GREEN, (self.col*W+W, self.row*W), (self.col*W+W, self.row*W+W+T), outlineThickness)
         if self.lines[2]:
+            #botttom
             pygame.draw.line(SCREEN, GREEN, (self.col*W, self.row*W+W), (self.col*W+W+T, self.row*W+W), outlineThickness)
         if self.lines[3]:
+            #left
             pygame.draw.line(SCREEN, GREEN, (self.col*W, self.row*W), (self.col*W, self.row*W+W+T), outlineThickness)
 def updateCanvas():
     SCREEN.fill(BLACK)
@@ -74,8 +78,11 @@ def updateCanvas():
         for j in range(COLS):
             cells[i][j].draw()
     pygame.display.update()
-    pygame.time.delay(50)  # Adjust delay for animation speed    
+    pygame.time.delay(5)  # Adjust delay for animation speed    
+    
 def removeWalls(currentCell, nextCell):
+
+    dy = currentCell.row - nextCell.row
     dx = currentCell.col - nextCell.col
 
     if dx == 1:
@@ -84,8 +91,6 @@ def removeWalls(currentCell, nextCell):
     elif dx == -1:
         currentCell.lines[1] = False
         nextCell.lines[3] = False
-
-    dy = currentCell.row - nextCell.row
     
     if dy == 1:
         currentCell.lines[0] = False
@@ -118,24 +123,8 @@ def main():
         while len(wallList) > 0:
             randomWall = random.choice(wallList)
 
-            if randomWall[1] == 1 and returnCellIndex(randomWall[0].row,randomWall[0].col+1):
-                rowIndex = randomWall[0].row
-                colIndex = randomWall[0].col
-                dividingCell = cells[rowIndex] [colIndex+1]
-
-                if randomWall[0].visited ^ dividingCell.visited:
-                    removeWalls(randomWall[0], dividingCell)
-
-                    if not(randomWall[0].visited):
-                        randomWall[0].visited = True
-                        print("There's somethings you're missing")
-                    if not(dividingCell.visited):
-                        dividingCell.visited = True
-                        wallList.append([dividingCell, 0])
-                        wallList.append([dividingCell, 1])
-                        wallList.append([dividingCell, 2])
-
-            if randomWall[1] == 2 and returnCellIndex(randomWall[0].row+1, randomWall[0].col):
+            if randomWall[1] == 2 and returnCellIndex(randomWall[0].row+1,randomWall[0].col):
+                # value == => bottom wall 
                 rowIndex = randomWall[0].row
                 colIndex = randomWall[0].col
                 dividingCell = cells[rowIndex+1][colIndex]
@@ -152,10 +141,29 @@ def main():
                         wallList.append([dividingCell, 2])
                         wallList.append([dividingCell, 3])
 
-            if randomWall[1] == 3 and returnCellIndex(randomWall[0].row, randomWall[0].col-1):
+            if randomWall[1] == 1 and returnCellIndex(randomWall[0].row, randomWall[0].col+1):
+                # value == => right wall 
                 rowIndex = randomWall[0].row
                 colIndex = randomWall[0].col
-                dividingCell = cells[rowIndex][colIndex-1]
+                dividingCell = cells[rowIndex][colIndex+1]
+
+                if randomWall[0].visited ^ dividingCell.visited:
+                    removeWalls(randomWall[0], dividingCell)
+
+                    if not(randomWall[0].visited):
+                        randomWall[0].visited = True
+                        print("There's somethings you're missing")
+                    if not(dividingCell.visited):
+                        dividingCell.visited = True
+                        wallList.append([dividingCell, 0])
+                        wallList.append([dividingCell, 1])
+                        wallList.append([dividingCell, 2])
+
+            if randomWall[1] == 0 and returnCellIndex(randomWall[0].row-1, randomWall[0].col):
+                # value == => top wall 
+                rowIndex = randomWall[0].row
+                colIndex = randomWall[0].col
+                dividingCell = cells[rowIndex-1][colIndex]
 
                 if randomWall[0].visited ^ dividingCell.visited:
                     removeWalls(randomWall[0], dividingCell)
@@ -165,9 +173,9 @@ def main():
                         print ("There's somethings you're missing")
                     if not(dividingCell.visited):
                         dividingCell.visited = True
-                        wallList.append([dividingCell, 0])
-                        wallList.append([dividingCell, 2])
                         wallList.append([dividingCell, 3])
+                        wallList.append([dividingCell, 0])
+                        wallList.append([dividingCell, 1])
 
             wallList.remove(randomWall)
             
